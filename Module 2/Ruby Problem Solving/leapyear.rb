09ad4a773042c76ea2dc6daaba8c1158577f67d0
Jimmy Hoang <jimmy.hoang@shopify.com>
2021-06-12 10:05:28 -0700

    more refactors

--- Module 2/Ruby Problem Solving/leapyear.rb
+++ Module 2/Ruby Problem Solving/leapyear.rb
@@ -1,22 +1,19 @@
 # Write a Ruby program which accept the a year from the user and determines if that year is a leap year or not. If they user enters anything other then a valid year, the application should re-prompt the user for to enter a valid year.
-print "Enter a year: "
-year = gets.chomp
-
-while true
-    if year.to_i.to_s == year && year.to_i > 0
-        break
-    else
-        puts "#{year} is not a valid year"
-        print "Enter an actual year: "
-        year = gets.chomp
-    end
+def get_year
+  print "Enter a year: "
+  return year = gets.chomp
 end
 
-year = year.to_i
+year = get_year
 
-if year % 4 == 0 
-  if year % 100 == 0
-    if year % 400 == 0
+until year.to_i.to_s == year && year.to_i > 0
+  puts "#{year} is not a valid year"
+  year = get_year
+end
+
+if year.to_i % 4 == 0 
+  if year.to_i % 100 == 0
+    if year.to_i % 400 == 0
       puts "#{year} is a leap year"
     else
       puts "#{year} is not a leap year"
--- Module 2/Ruby Problem Solving/max_number.rb
+++ Module 2/Ruby Problem Solving/max_number.rb
@@ -1,29 +1,16 @@
 # Write a Ruby program to find the maximum of two numbers. If something other than a number is entered, it should prompt the person again for the number until a number is entered.
+first_number = 0
+second_number = 0
 
-print "Enter a number:" 
-first_number = gets.chomp
-
-while true
-    if first_number.to_i.to_s == first_number
-        break
-    else
-        print "Enter an actual number: "
-        first_number = gets.chomp
-    end
+until first_number.to_i.to_s == first_number
+  print "Enter a number: "
+  first_number = gets.chomp
 end
 
-print "Enter another number: "
-second_number = gets.chomp
-
-while true
-    if second_number.to_i.to_s == second_number
-        break
-    else
-        print "Enter an actual number: "
-        second_number = gets.chomp
-    end
+until second_number.to_i.to_s == second_number
+  print "Enter another number: "
+  second_number = gets.chomp
 end
-
 
 max_number = [first_number, second_number].max
 
