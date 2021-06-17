be00e3e3ce0389305343baa768541741b63bfad1
Jimmy Hoang <jimmy.hoang@shopify.com>
2021-06-16 22:05:40 -0700

    re-prompts player to play again if tie or cheating

--- Module 2/rock-paper-scissors.rb
+++ Module 2/rock-paper-scissors.rb
@@ -1,45 +1,48 @@
 def play_game(player_one, player_two)
-
-    return "Tie" if player_one == player_two
-
     if player_one == "rock" && player_two == "paper"
-        puts "Paper covers rock."
-        puts "Player 2 wins."
+        yield("Paper covers rock.", "Player 2", false)
     elsif player_one == "rock" && player_two == "scissors"
-        puts "Rock crushes scissors"
-        puts "Player 1 wins."
+        yield("Rock crushes scissors", "Player 1", false)
     elsif player_one == "paper" && player_two == "scissors"
-        puts "Scissor cuts paper."
-        puts "Player 2 wins."
+        yield("Scissor cuts paper.", "Player 2", false)
     elsif player_one == "paper" && player_two == "rock"
-        puts "Paper covers rock."
-        puts "Player 1 wins."
+        yield("Paper covers rock.", "Player 1", false)
     elsif player_one == "scissors" && player_two == "paper"
-        puts "Scissors cuts paper."
-        puts "Player 1 wins."
+        yield("Scissors cuts paper.", "Player 1", false)
     elsif player_one == "scissors" && player_two == "rock"
-        puts "Rock crushes scissors"
-        puts "Player 2 wins."
+        yield("Rock crushes scissors", "Player 2", false)
+    elsif player_one == player_two
+        yield("Tie", nil, true)
     end
 end
-
 
 puts "Rock Paper Scissors"
 valid_answers = ["rock", "paper", "scissors"]
-cheat_message = "No cheaters! Only Rock, Paper, or Scissors are allowed."
+play_again = true
 
-print "Player 1 - Enter your selection: "
-player_one = gets.chomp.downcase
+while play_again
+    player_one = ""
+    player_two = ""
 
-print "Player 2 - Enter your selection: "
-player_two = gets.chomp.downcase
+    until (valid_answers.include? player_one) && (valid_answers.include? player_two)
+        print "Player 1 - Enter your selection: "
+        player_one = gets.chomp.downcase
+    
+        print "Player 2 - Enter your selection: "
+        player_two = gets.chomp.downcase
 
-if valid_answers.include? player_one
-    if valid_answers.include? player_two
-        puts play_game(player_one, player_two)
-    else
-        puts cheat_message
+        unless (valid_answers.include? player_one) && (valid_answers.include? player_two)
+            puts "No cheaters! Only Rock, Paper, or Scissors are allowed."
+        end
+
     end
-else
-    puts cheat_message
+
+    play_game(player_one, player_two) do | text, winner, again |
+        puts text
+        puts "#{winner} wins!" if winner
+        play_again = again
+    end
 end
+
+
+
