3e0bb08894f46956710ac32e64284da390ebcf51
Jimmy Hoang <jimmy.hoang@shopify.com>
2021-06-29 15:48:29 -0700

    finished code along

--- Module 3/Code Along/reviews.rb
+++ Module 3/Code Along/reviews.rb
@@ -4,9 +4,7 @@ def find_adjective(string)
     words[index + 1]
 end
 
-
 lines = []
-
 File.open("reviews.txt") do |review_file|
     lines = review_file.readlines
 end
@@ -14,8 +12,10 @@ end
 relevant_lines = lines.find_all { |line| lines.include?("Truncated") }
 reviews = relevant_lines.reject { |line| lines.include?("--") }
 
-adjectives = []
-
-reviews.each do |review|
-    adjectives << find_adjective(review)
+adjectives = reviews.map do |review| 
+    adjective = find_adjective(review) 
+    "'#{adjective.capitalize}'"
 end
+
+puts "The critics agree, Truncated is:"
+puts adjectives
