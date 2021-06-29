6c83fcc3cb3d04790257f69314f9f5fd325d7d20
Jimmy Hoang <jimmy.hoang@shopify.com>
2021-06-29 15:22:34 -0700

    only save relevant lines

--- Module 3/Code Along/reviews.rb
+++ Module 3/Code Along/reviews.rb
@@ -3,3 +3,11 @@ lines = []
 File.open("reviews.txt") do |review_file|
     lines = review_file.readlines
 end
+
+relevant_lines = []
+
+lines.each do |line|
+    if line.include?("Truncated")
+        relevant_lines << line
+    end
+end
