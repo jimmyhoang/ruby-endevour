70032097bc2383631a617128dd6ccb97a1cf707a
Jimmy Hoang <jimmy.hoang@shopify.com>
2021-07-02 15:24:46 -0700

    finished multiples assignment

--- Module 4/multiples.rb
+++ Module 4/multiples.rb
@@ -1,8 +1,22 @@
 def customized_list_of_multiples(multiples_of, size)
-    list = Array.new(size)
+    list = (1..size).to_a.map { |x| multiples_of * x}
+
     if block_given?
-        yield
+        list = yield(list) 
+        print list
     else
         list
+        print list
     end
+
+end
+
+customized_list_of_multiples(2, 4)
+
+customized_list_of_multiples(5, 5) do |list| 
+    list.map { |y| y * 10 }
+end
+
+customized_list_of_multiples(10, 6) do |list|
+    list.map { |y| y / 2 }
 end
